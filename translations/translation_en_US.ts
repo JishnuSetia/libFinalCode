2cedba8dcc593f3a5e40fce676e11cc82d5877f9
Jishnu Setia <jishnusetia8@gmail.com>
2023-11-05 12:22:13 +0400

    commit1

---
+++ translations/translation_en_US.ts
@@ -0,0 +1,299 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I have some suggestions for you to imrove your lexile level. You may choose The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you ready to choose now?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">I have some suggestions for you to imrove your lexile level. You may choose The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you ready to choose now?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (10)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (11)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (12)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (13)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (14)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (15)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (16)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (17)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (18)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>To raise your lexile level, I have some suggestions for you.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">To raise your lexile level, I have some suggestions for you.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You can read The cat in the hat by Doctor Seuss, The Princess in Black by Shannon Hale, The BFG by Roald Dahl,Adventure series by Enid Blyton, Holes by Louis Sachar, Scarlet and Ivy by Sophie Cleverly, The 1000-Year-old Boy by Ross Welford, Artemis Fowl by Eoin Colfer or The one and only Ivan by Katherine Applegatehave. Are you prepared to make a decision now?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">There is Wonder by R J Palacio, Alex Rider series by Anthony Horovitz, Narnia series by C S Lewis, Inside Out &amp; Back Again by Thanhha Lai, Wish by Barbara O'Connor, An eagle in the snow by Michael Morpurgo, I, Robot by Isaac Asimov to choose from. Ready to make a decision?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You may choose The Jungle Books by Rudyard Kipling, The Hobbit by J R R Tolkien, A Tale of Two Cities by Charles Dickens or Little Women by Louisa May Alcott. Ready to choose?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (9)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I have some suggestions for you to imrove your lexile level.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I have some suggestions for you to imrove your lexile level.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good, below</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good, below</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good above</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good above</translation>
+        </message>
+        <message>
+            <source>Good </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+        <message>
+            <source>Good middle</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good middle</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Good</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Good</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Good</translation>
+        </message>
+    </context>
+</TS>
